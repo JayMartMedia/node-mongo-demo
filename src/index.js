556c9ee27a78220fea82c85ec2026bdc20c2b563
JayMartMedia <Jay@JayMartMedia.com>
2021-02-22 23:36:37 -0500

    Added some preset functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-var mongo = require('mongodb');
+const mongo = require('mongodb');
+
+const url = "mongodb://localhost:27017/roles";
+
+mongo.MongoClient.connect(url, (err, db) => {
+    if(err) throw err;
+    const dbo = db.db("roles");
+    retrieveAllObjects(dbo);
+});
+
+const insertObject = async (dbo, obj) => {
+    dbo.collection("emoji_role").insertOne(obj, (err, res) => {
+        if (err) throw err;
+    })
+}
+
+const retrieveObject = async (dbo, query) => {
+    dbo.collection("emoji_role").findOne(query, (err, result) => {
+        if (err) throw err;
+        console.log(result)
+        return result;
+    })
+}
+
+const retrieveAllObjects = async (dbo) => {
+    dbo.collection("emoji_role").find({}).toArray((err, result) => {
+        if (err) throw err;
+        console.log(result);
+        return result;
+    });
+}
+
+const deleteByRole = async (dbo, role) => {
+    dbo.collection("emoji_role").deleteMany({"role" : role}, (err, obj) => {
+        if (err) throw err;
+        console.log(obj.result.n + " document(s) deleted");
+    });
+}
